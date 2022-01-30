d7df3b3b3297e7d7585d844568c7a4baea5ddf78
Robert <robertbarnett06@gmail.com>
2022-01-30 13:44:17 -0800

    rework

---
+++ main.py
@@ -0,0 +1,33 @@
+
+import discord
+intents = discord.Intents.all()
+import os
+import requests
+import json
+from decouple import config
+#from apikeys import TOKEN
+#from replit import db
+from discord.ext import commands
+
+activity = discord.Activity(type=discord.ActivityType.watching, name="hentai")
+client = commands.Bot(command_prefix = '$', activity=activity, status=discord.Status.online, intents=intents)
+
+@client.event
+async def on_ready():
+  print("We have logged in as {0.user}".format(client))
+
+@client.command()
+async def hello(ctx):
+  await ctx.send("hello, i hate league")
+
+
+@client.event
+async def on_member_update(before, after):
+    games = ["league of legends"]
+    if after.activity and after.activity.name.lower() in games:
+            channel = client.get_channel(886329545551130664)
+            await channel.send(f"{after.mention} league :sick:")
+
+#keep_alive()
+
+client.run(config('TOKEN'))
