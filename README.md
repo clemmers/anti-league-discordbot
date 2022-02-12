052cf564837d756132cf1eb93896cbb6f0b9f088
Chris Clem <chrisclem244@yahoo.com>
2022-02-11 21:16:41 -0800

    made bot alot nicer

--- README.md
+++ README.md
@@ -3,18 +3,11 @@
 <br>
 harrasses imbeciles for playing league of legends
 
+***To invite the bot into your sever, paste the following link into your browser***
+<code>https://discord.com/api/oauth2/authorize?client_id=937062519317598248&permissions=8&scope=bot</code>
 
-## Running
-
-First set TOKEN and GUILD_ID in the .env file<br>
-Ensure that Presence Intent and Server Members Intent are enabled
-in the bot page of your discord developer portal
-
-Then execute <code>run.bat</code> or <code>run.sh</code>
-
-
-On startup, the bot will create a channel called "channel-of-shame" if not
-already created. In that channel, the bot will harrass anyone who
+When joining a server, the bot will create a channel called "channel-of-shame"
+if not already created. In that channel, the bot will harrass anyone who
 is playing league and has Activity Status enabled.
 (By default, bot will @ the league player with a message insulting
 them for playing league)
@@ -22,6 +15,26 @@ them for playing league)
 
 ## Commands 
 *You need server administrator to run commands*<br><br>
+
 **$zeroTolerance** enable/disable - if enabled, anyone who plays league
-will be permanently banned from the server. Disabled by default.
+will be permanently banned from the server. Disabled by default.<br><br>
+
+**$games** add/remove "game name" - Edits the list of games that the bot
+will yell at you for playing *$games view* will print a list of all of
+the games.<br><br>
+
+**$resetSettingsToDefault** - resets config file to its default state
+(This resets all unique settings, such as custom games and messages)
+
+## Hosting
+
+*If you wish to host this bot by yourself, follow below instructions*<br>
+First set TOKEN in the .env file<br>
+Ensure that Presence Intent and Server Members Intent are enabled
+in the bot page of your discord developer portal
+
+Install the following packages using pip
+<code>pip install python-decouple</code>
+<code>pip install discord</code>
+Then execute your main.py
 
--- main.py
+++ main.py
@@ -1,36 +1,80 @@
 import discord
+import json
+import os
+
 intents = discord.Intents.all()
 from decouple import config
 from discord.ext import commands
 
 activity = discord.Activity(type=discord.ActivityType.playing, name="LITERALLY ANY OTHER GAME")
 client = commands.Bot(command_prefix = '$', activity=activity, status=discord.Status.online, intents=intents)
-global channel
-global guild
-tolerance = True
+
+
 
 @client.event
 async def on_ready():
   print("We have logged in as {0.user}".format(client))
   print("Branch version " + discord.__version__)
-  global guild
-  guild = client.get_guild(int(config('GUILD_ID')))
-  await createChannel(guild)
 
 async def createChannel(guild):
-  global channel
+  
   for i in guild.channels:
     if(i.name == 'channel-of-shame'):
       channel = i
-      print("Channel of Shame is already created, but got its channel ID!")
-      break
+      return channel
   else:
     channel = await guild.create_text_channel('channel-of-shame')
     await channel.set_permissions(guild.default_role, send_messages=False)
+    return channel
   
+
+@client.command()
+async def games(ctx, style = " ", game = " "):
+  if style.lower() == 'add' and game != " ":
+    with open(f'{os.getcwd()}/guilds/{ctx.guild.id}.json', 'r') as f:
+      data = json.load(f)
+      if game.lower() not in data['games']:
+        data['games'].append(game.lower())
+        await ctx.send(game + " added to list :thumbsup:")
+      else:
+        await ctx.send(game + " is already on the list!")
+        return
+    with open(f'{os.getcwd()}/guilds/{ctx.guild.id}.json', 'w') as f:
+      json.dump(data, f, indent=4)
+  elif style.lower() == 'remove' and game != " ":
+    with open(f'{os.getcwd()}/guilds/{ctx.guild.id}.json', 'r') as f:
+      data = json.load(f)
+    if game.lower() in data['games']:
+      data['games'].remove(game.lower())
+      await ctx.send(game + " removed from the list")
+    else:
+      await ctx.send(game + " is not in the list!")
+      return
+    with open(f'{os.getcwd()}/guilds/{ctx.guild.id}.json', 'w') as f:
+      json.dump(data, f, indent=4)
+  elif style.lower() == 'view':
+    f = open(f'{os.getcwd()}/guilds/{ctx.guild.id}.json')
+    data = json.load(f)
+    await ctx.send(data.get('games'))
+  else:
+    await ctx.send("Command used incorrectly. Please specify whether you want to 'add', 'remove', or 'view' game list.")
+
+
+@client.command()
+async def resetSettingsToDefault(ctx):
+  try:
+    os.remove(f'{os.getcwd()}/guilds/{ctx.guild.id}.json')
+  except OSError:
+    pass
+  await createConfig(ctx.guild)
+  await ctx.send("Config file succesfully reset")
+
+
 @client.command()
 async def zeroTolerance(ctx, boo=" "):
-  global tolerance
+  f = open(f'{os.getcwd()}/guilds/{ctx.guild.id}.json')
+  data = json.load(f)
+  tolerance = data.get('tolerance')
   if not ctx.message.author.guild_permissions.administrator:
     await ctx.send("no")
     return
@@ -40,6 +84,7 @@ async def zeroTolerance(ctx, boo=" "):
     
   elif boo.lower() == 'enable':
     await ctx.send("You already have Zero Tolerance enabled!")
+    return
     
   elif boo.lower() == 'disable' and tolerance == False:
     await ctx.send("Zero Tolerance disabled!")
@@ -47,31 +92,75 @@ async def zeroTolerance(ctx, boo=" "):
     
   elif boo.lower() == 'disable':
     await ctx.send("Zero Tolerance was not enabled so it was not disabled.")
+    return
     
   elif boo.lower() == " ":
     await ctx.send("Zero Tolerance is " + str(tolerance).lower() + ". If you wish to change that, please specify whether you want to 'enable' or 'disable' zero tolerance")
+    return
     
   else:
     await ctx.send("Not a valid input. Please say 'enable' for zero tolerance, or 'disable' if you want to be merciful")
+    return
+  
+  with open(f'{os.getcwd()}/guilds/{ctx.guild.id}.json', 'r') as f:
+    data = json.load(f)
+    data['tolerance'] = tolerance 
+  with open(f'{os.getcwd()}/guilds/{ctx.guild.id}.json', 'w') as f:
+    json.dump(data, f, indent=4)
+
 
 @client.event
 async def on_member_update(before, after):
-    global guild
-    games = ["league of legends"]
-    if after.activity and after.activity.name.lower() in games and after in guild.members:
-      if before.activity is None:
-        await punish(after)
-      elif not before.activity.name.lower() in games:
-        await punish(after)
+  try:
+    f = open(f'{os.getcwd()}/guilds/{after.guild.id}.json')
+  except FileNotFoundError:
+    await createConfig(after.guild)
+    f = open(f'{os.getcwd()}/guilds/{after.guild.id}.json')
+  data = json.load(f)
+  games = data.get('games')
+  if after.activity and after.activity.name.lower() in games:
+    
+    if before.activity is None:
+      await punish(after, data)
+    elif not before.activity.name.lower() in games:
+      await punish(after, data)
 
-async def punish(after):
-  global channel
-  global tolerance
+
+async def punish(after, data):
+  tolerance = data.get('tolerance')
+  channel = client.get_channel(data.get('channelID'))
+  messages = data.get('messages')
   if tolerance:
-    await channel.send(f"{after.mention} is playing league :sick:")
+    await channel.send(f"{after.mention} is playing {after.activity.name.lower()} :sick:")
   else:
     await channel.send(f"{after.mention} plays league!!!! BANNED :sick::sick::sick:")
     await after.ban(reason = "ZERO TOLERANCE FOR LEAGUE PLAYERS")
 
+
+@client.event
+async def on_guild_remove(guild):
+  try:
+    os.remove(f'{os.getcwd()}/guilds/{guild.id}.json')
+  except OSError:
+    pass
+
+
+@client.event
+async def on_guild_join(guild):
+  await createConfig(guild)
+
+
+async def createConfig(guild):
+  channel = await createChannel(guild)
+  default = {
+    "games" : ["league of legends"],
+    "tolerance" : True,
+    "messages" : ["{after.mention} is playing {after.activity.name.lower()} :sick:"],
+    "channelID" : channel.id
+  }
+
+  json_object = json.dumps(default, indent = 4)
+  with open(f"{os.getcwd()}/guilds/{guild.id}.json", "w") as outfile:
+    outfile.write(json_object)
 
 client.run(config('TOKEN'))
